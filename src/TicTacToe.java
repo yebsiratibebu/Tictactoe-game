9f243fec894bcd93ef30ed8a9ab1a353675c6680
Yebsira Tibebu <yebsiratibebu@Yebsiras-MacBook-Air.local>
2024-12-20 16:55:03 -0600

    created my files and my frame

---
+++ src/TicTacToe.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TicTacToe {
+    int width = 600;
+    int height = 650; 
+
+    JFrame f = new JFrame();
+    public TicTacToe () {
+
+        f.setVisible(true);
+        f.setSize(width, height);
+        f.setTitle(TicTacToe);
+    
+    }
+}
