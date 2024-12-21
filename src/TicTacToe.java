1f1a81a567825363b138b8be7568ed8dbc180c09
Yebsira Tibebu <yebsiratibebu@Yebsiras-MacBook-Air.local>
2024-12-21 16:52:10 -0600

    added a layout and text

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,11 +7,28 @@ public class TicTacToe {
     int height = 650; 
 
     JFrame f = new JFrame();
+    JLabel text = new JLabel();
+    JPanel Dpanel = new JPanel();
+
     public TicTacToe () {
 
         f.setVisible(true);
         f.setSize(width, height);
-        f.setTitle(TicTacToe);
+        //f.setTitle(TicTacToe);
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLayout(new BorderLayout());
+
+        text.setBackground(Color.darkGray);
+        text.setForeground(Color.white);
+        text.setFont(new Font( "Times New Roman", Font.BOLD, 50));
+        text.setHorizontalAlignment(JLabel.CENTER);
+        text.setText("TIC-TAC-TOE");
+
+        Dpanel.setLayout(new BorderLayout());
+        Dpanel.add(text);
+        f.add(Dpanel);
+
     
     }
 }
